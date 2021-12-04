a25ed668c114be7f43a9f88ca9dee7ee19ef43d4
dawiddmitruk <adres.do.korespondencji@o2.pl>
2021-12-04 15:25:07 +0100

    "Siema" na gorze ekranu logowania

--- src/main/java/bb/dd/dp/przeplyw_dokumentow_projekt_gui/MainController.java
+++ src/main/java/bb/dd/dp/przeplyw_dokumentow_projekt_gui/MainController.java
@@ -3,8 +3,10 @@ package bb.dd.dp.przeplyw_dokumentow_projekt_gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
@@ -21,8 +23,7 @@ public class MainController {
 	
 	@FXML
 	void initialize() {
-		var siema = new AnchorPane(new Text("Siema"));
-		siema.setPrefHeight(40);
+		var siema = new Text("Siema");
 		
 		mainPane.setTop(siema);
 	}
