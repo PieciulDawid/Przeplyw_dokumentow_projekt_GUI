181b7b97cab138263daeca65089ae1d174d45620
Dawid Pieciul <pieciul.dawid@gmail.com>
2021-12-05 19:57:48 +0100

    Kolorki tła

--- src/main/java/bb/dd/dp/przeplyw_dokumentow_projekt_gui/Controllers/MainController.java
+++ src/main/java/bb/dd/dp/przeplyw_dokumentow_projekt_gui/Controllers/MainController.java
@@ -2,6 +2,8 @@ package bb.dd.dp.przeplyw_dokumentow_projekt_gui.Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.layout.*;
@@ -18,12 +20,40 @@ public class MainController {
 	
 	@FXML
 	private BorderPane mainPane;
+
+	private AnimationTimer animationTimer;
+
+	private int i=0;
+	private int count = 0;
+
+	private Background colors[] = new Background[190];
 	
 	@FXML
 	void initialize() {
-//		var siema = new Text("Siema");
-//		var backgroundColor = new Background(new BackgroundFill(Color.CYAN, CornerRadii.EMPTY, Insets.EMPTY));
-//		mainPane.setBackground(backgroundColor);
-//		mainPane.setTop(siema);
+		var siema = new Text("Siema");
+		var backgroundColor = new Background(new BackgroundFill(Color.CYAN, CornerRadii.EMPTY, Insets.EMPTY));
+		mainPane.setBackground(backgroundColor);
+		mainPane.setTop(siema);
+
+		for (int i = 160; i<255; i++) {
+			colors[i-160] = new Background(new BackgroundFill(Color.color(0, (float) i / 255, 1), CornerRadii.EMPTY, Insets.EMPTY));
+		}
+		for (int i = 255; i>160; i--) {
+			colors[190+160-i] = new Background(new BackgroundFill(Color.color(0, (float)i / 255, 1), CornerRadii.EMPTY, Insets.EMPTY));
+		}
+
+		animationTimer = new AnimationTimer() {
+			@Override
+			public void handle(long l) {
+				count = (count+1)%7;
+				if(count ==0)
+					changeBackground();
+			}
+		};
+		animationTimer.start();
+	}
+	void changeBackground(){
+		i = (i+1)%190;
+		mainPane.setBackground(colors[i]);
 	}
 }
--- src/main/java/bb/dd/dp/przeplyw_dokumentow_projekt_gui/HelloApplication.java
+++ src/main/java/bb/dd/dp/przeplyw_dokumentow_projekt_gui/HelloApplication.java
@@ -18,49 +18,18 @@ public class HelloApplication extends Application {
 	@Override
 	public void start(Stage stage) throws IOException {
 		stage.setResizable(false);
-		
+
 		var mainLoader = new FXMLLoader(HelloApplication.class.getResource("main-view.fxml"));
-		var leftLoader = new FXMLLoader(HelloApplication.class.getResource("leftpane-view.fxml"));
-		var bottomLoader = new FXMLLoader(HelloApplication.class.getResource("bottompane-view.fxml"));
+		var loginLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml"));
 
 		var scene = new Scene(mainLoader.load());
-		((BorderPane)scene.getRoot()).setLeft(leftLoader.load());
-		((BorderPane)scene.getRoot()).setBottom(bottomLoader.load());
-
-		var table = new TableView<EmployeeModel>();
-		table.setEditable(true);
-		var namecol = new TableColumn("Imie");
-		var surnamecol = new TableColumn("Nazwisko");
-		var logincol = new TableColumn("Login");
-		var passwordcol = new TableColumn("Haslo");
-		namecol.setPrefWidth(225);
-		namecol.setCellValueFactory(
-				new PropertyValueFactory<EmployeeModel,String>("Name")
-		);
-		surnamecol.setPrefWidth(225);
-		surnamecol.setCellValueFactory(
-				new PropertyValueFactory<EmployeeModel,String>("Surname")
-		);
-		logincol.setPrefWidth(225);
-		logincol.setCellValueFactory(
-				new PropertyValueFactory<EmployeeModel,String>("Login")
-		);
-		passwordcol.setPrefWidth(225);
-		passwordcol.setCellValueFactory(
-				new PropertyValueFactory<EmployeeModel,String>("Password")
-		);
-
-		table.getColumns().addAll(namecol, surnamecol, logincol, passwordcol);
-
-		final ObservableList<EmployeeModel> data = FXCollections.observableArrayList(EmployeeModel.getAll().values());
-		table.setItems(data);
-		((BorderPane)scene.getRoot()).setCenter(table);
+		((BorderPane)scene.getRoot()).setCenter(loginLoader.load());
 
 		var stylesheets = scene.getStylesheets();
-		
+
 		//	TODO css do dodania gdy uda się w nim ogarnąć
 		//	 jak dodać widoczność klikania przycisków
-		//stylesheets.add("stylesheet.css");
+		stylesheets.add("stylesheet.css");
 		stage.setTitle("BB DD DP");
 		stage.setScene(scene);
 		stage.show();
